d59e680293e2c0e8916099ec160377a0219ba000
Andre Teles <andre.telestp@gmail.com>
2020-09-09 08:40:46 -0300

    chore: initial commit

---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+import GlobalStyles from 'styles/global'
+import { ThemeProvider } from 'styled-components'
+import theme from 'styles/theme'
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  )
+]
