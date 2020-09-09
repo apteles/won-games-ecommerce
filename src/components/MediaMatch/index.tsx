6bc1f376c4ad8a3d039e6f1d5ed2e835980ebda9
Andre Teles <andre.telestp@gmail.com>
2020-09-09 09:30:57 -0300

    feat: add component mediaMatch

---
+++ src/components/MediaMatch/index.tsx
@@ -0,0 +1,25 @@
+import styled, { css } from 'styled-components'
+import media, { DefaultBreakpoints } from 'styled-media-query'
+
+type breakpoints = keyof DefaultBreakpoints
+export type MediaMatchProps = {
+  lessThan?: breakpoints
+  greaterThan?: breakpoints
+}
+
+const mediaMatchModifier = {
+  lessThan: (size: breakpoints) => css`
+    ${media.lessThan(size)`display: block`}
+  `,
+  greaterThan: (size: breakpoints) => css`
+    ${media.greaterThan(size)`display: block`}
+  `
+}
+
+export default styled.div<MediaMatchProps>`
+  ${({ lessThan, greaterThan }) => css`
+    display: none;
+    ${!!lessThan && mediaMatchModifier.lessThan(lessThan)}
+    ${!!greaterThan && mediaMatchModifier.greaterThan(greaterThan)}
+  `}
+`
