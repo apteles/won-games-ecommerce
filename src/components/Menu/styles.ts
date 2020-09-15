d35360260e02651eeb7c839b9b11755ffbeeaf5d
Andre Teles <andre.telestp@gmail.com>
2020-09-15 17:00:53 -0300

    chore: create menu full

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,6 +1,9 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
+type menuFullProps = {
+  isOpen: boolean
+}
 export const Wrapper = styled.menu`
   ${({ theme }) => css`
     display: flex;
@@ -8,6 +11,79 @@ export const Wrapper = styled.menu`
     align-items: center;
     justify-content: space-between;
     position: relative;
+  `}
+`
+export const MenuFull = styled.nav<menuFullProps>`
+  ${({ isOpen, theme }) => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background: ${theme.colors.white};
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 100vh;
+    overflow: hidden;
+    opacity: ${isOpen ? 1 : 0};
+    pointer-events: ${isOpen ? 'all' : 'none'};
+
+    > svg {
+      position: absolute;
+      top: 0;
+      right: 0;
+      margin: ${theme.spacings.xsmall};
+      cursor: pointer;
+      width: 2.4rem;
+      height: 2.4rem;
+    }
+
+    ${MenuLink} {
+      color: ${theme.colors.black};
+      font-weight: ${theme.font.bold};
+      font-size: ${theme.font.sizes.xlarge};
+      margin-bottom: ${theme.spacings.small};
+    }
+    ${MenuNav} {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      flex: 1;
+    }
+  `}
+`
+export const MenuNav = styled.div``
+export const MenuLink = styled.a`
+  ${({ theme }) => css`
+    position: relative;
+    font-size: ${theme.font.sizes.medium};
+    margin: 0.3rem ${theme.spacings.small} 0;
+    text-decoration: none;
+    text-align: center;
+
+    &:hover {
+      &::after {
+        content: '';
+        position: absolute;
+        display: block;
+        height: 0.3rem;
+        background-color: ${theme.colors.primary};
+        animation: hoverAnimation 0.2s forwards;
+      }
+
+      @keyframes hoverAnimation {
+        from {
+          width: 0;
+          left: 50%;
+        }
+        to {
+          width: 100%;
+          left: 0;
+        }
+      }
+    }
   `}
 `
 
@@ -37,5 +113,28 @@ export const MenuGroup = styled.div`
     > div {
       margin-left: ${theme.spacings.xsmall};
     }
+  `}
+`
+
+export const RegisterBox = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 ${theme.spacings.xlarge} ${theme.spacings.xlarge};
+
+    > span {
+      display: block;
+      margin: ${theme.spacings.xxsmall} 0;
+      font-size: ${theme.font.sizes.xsmall};
+    }
+  `}
+`
+
+export const CreateAccount = styled.a`
+  ${({ theme }) => css`
+    text-decoration: none;
+    color: ${theme.colors.primary};
+    border-bottom: 0.2rem solid ${theme.colors.primary};
   `}
 `
