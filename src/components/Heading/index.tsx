80b10eba48903690ed89ffe94ce17a468ca115b0
Andre Teles <andre.telestp@gmail.com>
2020-09-22 11:26:56 -0300

    chore: refactor heading

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,19 +1,30 @@
 import * as S from './styles'
 
+export type LineColor = 'primary' | 'secondary'
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  size?: 'small' | 'medium'
+  lineColor?: LineColor
 }
 
 const Heading = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  size = 'medium',
+  lineColor = 'primary'
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <S.Wrapper
+    color={color}
+    lineLeft={lineLeft}
+    size={size}
+    lineBottom={lineBottom}
+    lineColor={lineColor}
+  >
     {children}
   </S.Wrapper>
 )
