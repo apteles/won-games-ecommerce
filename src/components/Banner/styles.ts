dac2a1b8ad622e9413b4d6654efcc528c542c388
Andre Teles <andre.telestp@gmail.com>
2020-10-13 19:10:07 -0300

    chore: add structure banner

---
+++ src/components/Banner/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main``
+
+export const Image = styled.div<{ src: string }>``
+
+export const Caption = styled.div``
+
+export const Title = styled.h2``
+
+export const Subtitle = styled.h3``
