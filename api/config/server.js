031a7860044448728c973df6621a67da78abaecf
Andre Teles <andre.telestp@gmail.com>
2020-11-12 18:18:08 -0300

    refactor project

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f1d812940698ca11afd58d6ebb8e96a0'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'be3a5609-729c-4162-8bf7-8284738825fa'
+};
