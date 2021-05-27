574d31abf2a6f5c27e52de295b885385cf74f29c
Andre Teles <andre.telestp@gmail.com>
2021-05-27 15:21:53 -0300

    chore: add structure of auth template and styles closes #11

--- frontend/src/pages/templates/Auth/styles.ts
+++ frontend/src/pages/templates/Auth/styles.ts
@@ -1,3 +1,11 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main``
+
+export const BannerBlock = styled.div``
+
+export const Subtitle = styled.h3``
+
+export const Footer = styled.p``
+
+export const ContentRight = styled.div``
